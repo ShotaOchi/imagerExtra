fb44823f6c24cec7998fbc1a9aeba374f45552f3
Shota Ochi <shotaochi1990@gmail.com>
2018-07-21 06:52:48 +0900

    isolated documentation of data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+
+#' Photograph of a dog from GAHAG
+#'
+#' This photograph was downloaded from http://gahag.net/img/201603/03s/gahag-0062116383-1.jpg.
+#' Its size was reduced by half to speed up loading and save space.
+#' @format an image of class cimg
+#' @source \url{http://gahag.net/img/201603/03s/gahag-0062116383-1.jpg}
+"dogs"
+
+#' Photograph of a paper
+#' 
+#' This photograph was filmed by Shota Ochi.
+#' @format an image of class cimg
+"papers"
--- R/misc.R
+++ R/misc.R
@@ -34,17 +34,3 @@ NULL
 #' @importFrom tesseract ocr_data
 #' @importFrom tesseract tesseract
 NULL
-
-#' Photograph of a dog from GAHAG
-#'
-#' This photograph was downloaded from http://gahag.net/img/201603/03s/gahag-0062116383-1.jpg.
-#' Its size was reduced by half to speed up loading and save space.
-#' @format an image of class cimg
-#' @source \url{http://gahag.net/img/201603/03s/gahag-0062116383-1.jpg}
-"dogs"
-
-#' Photograph of a paper
-#' 
-#' This photograph was filmed by Shota Ochi.
-#' @format an image of class cimg
-"papers"
