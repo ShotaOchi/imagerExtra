f433075586d2c38ebb5fcefefa8991bd85f3d90f
Shota Ochi <shotaochi1990@gmail.com>
2018-10-25 04:28:16 +0900

    implemented multilevel thresholding

--- R/multilevel_thresholding.R
+++ R/multilevel_thresholding.R
@@ -1,21 +1,20 @@
-#' Multilevel Thresholding (Maximum Entropy Based Artificial Bee Colony Thresholding)
-#'
-#' automatic multilevel thresholding based on Maximum Entropy Based Artificial Bee Colony Thresholding
-#' @param im a grayscale image of class cimg
-#' @param k the number of thresholds
-#' @param sn population size
-#' @param mcn maximum cycle number
-#' @param limit abandonment criteria
-#' @param intervalnumber interval number of histogram
-#' @param returnvalue if returnvalue is TRUE, returns threshold values. if FALSE, returns a grayscale image of class cimg.
-#' @return a grayscale image of class cimg or a numeric vector
-#' @references Ming-HuwiHorng (2011). Multilevel thresholding selection based on the artificial bee colony algorithm for image segmentation. Expert Systems with Applications.
-#' @author Shota Ochi
-#' @export
-#' @examples
-#' g <- grayscale(boats)
-#' ThresholdML(g, 2) %>% plot
-ThresholdML <- function(im, k, sn = 30, mcn = 100, limit = 100, intervalnumber = 1000, returnvalue = FALSE)
+#$' Automatic Multilevel Thresholding (Maximum Entropy Based Artificial Bee Colony Thresholding)
+#$'
+#$' automatic multilevel thresholding based on Maximum Entropy Based Artificial Bee Colony Thresholding
+#$' @param im a grayscale image of class cimg
+#$' @param k level of thresholding
+#$' @param sn population size
+#$' @param mcn maximum cycle number
+#$' @param limit abandonment criteria
+#$' @param intervalnumber interval number of histogram
+#$' @param returnvalue if returnvalue is TRUE, returns threshold values. if FALSE, returns a grayscale image of class cimg.
+#$' @return a grayscale image of class cimg or a numeric vector
+#$' @references Ming-HuwiHorng (2011). Multilevel thresholding selection based on the artificial bee colony algorithm for image segmentation. Expert Systems with Applications.
+#$' @author Shota Ochi
+#$' @examples
+#$' g <- grayscale(boats)
+#$' ThresholdML(g, 2) %>% plot
+ThresholdML_MEABCT <- function(im, k, sn = 30, mcn = 100, limit = 100, intervalnumber = 1000, returnvalue = FALSE)
 {
   CheckSanityim(im)
   CheckSanitypositivenumeric(k, "k")
@@ -64,4 +63,62 @@ ThresholdML <- function(im, k, sn = 30, mcn = 100, limit = 100, intervalnumber =
     return(thresvals)
   }
   return(as.cimg(threshold_multilevel(as.matrix(im), thresvals)))
+}
+
+#' Multilevel Thresholding
+#'
+#' multilevel thresholding
+#' @param im a grayscale image of class cimg
+#' @param k level of thresholding. k is ignored when thr is a numeric vector.
+#' @param thr thresholds, either numeric vector, or "fast", or "precise", or "manual"
+#' @param sn population size. sn is ignored except when thr is "manual".
+#' @param mcn maximum cycle number. mcn is ignored except when thr is "manual".
+#' @param limit abandonment criteria. limit is ignored except when thr is "manual".
+#' @param intervalnumber interval number of histogram. intervalnumber is ignored except when thr is "manual".
+#' @param returnvalue if returnvalue is TRUE, returns threshold values. if FALSE, returns a grayscale image of class cimg.
+#' @return a grayscale image of class cimg or a numeric vector
+#' @references Ming-HuwiHorng (2011). Multilevel thresholding selection based on the artificial bee colony algorithm for image segmentation. Expert Systems with Applications.
+#' @author Shota Ochi
+#' @export
+#' @examples
+#' g <- grayscale(boats)
+#' ThresholdML(g, k = 2) %>% plot
+ThresholdML <- function(im, k, thr = "fast", sn = 30, mcn = 100, limit = 100, intervalnumber = 1000, returnvalue = FALSE)
+{
+  res <- NULL
+  CheckSanityim(im)
+  if (is.character(thr))
+  {
+    CheckSanitychar(thr, "thr")
+    if (thr == "fast")
+    {
+      res <- ThresholdML_MEABCT(im, k, 30, 100, 100, 1000, returnvalue)
+    } else if (thr == "precise")
+    {
+      res <- ThresholdML_MEABCT(im, k, 100, 200, 10, 2000, returnvalue)
+    } else if (thr == "manual")
+    {
+      res <- ThresholdML_MEABCT(im, k, sn, mcn, limit, intervalnumber, returnvalue)
+    } else 
+    {
+      stop("thr must be a numeric vector, or 'fast', or 'precise', or 'manual'.", call. = FALSE)
+    } 
+  } else if (is.numeric(thr))
+  {
+    CheckSanitynumericvec(thr, "thr")
+    ordered <- thr[order(thr)]
+    if (any(ordered != thr))
+    {
+      warning("thr was arranged in ascending order.", call.=FALSE)
+    }
+    if (returnvalue)
+    {
+      return(ordered)
+    }
+    res <- as.cimg(threshold_multilevel(as.matrix(im), ordered))
+  } else
+  {
+    stop("thr must be a numeric vector, or 'fast', or 'precise', or 'manual'.", call. = FALSE)
+  }  
+  return(res)
 }
