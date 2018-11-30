6000fd2bfb0d9c18a2d2c6b2b75cfeee4808e24d
Shota Ochi <shotaochi1990@gmail.com>
2018-11-30 12:48:35 +0900

    improved explanation of imagerExtra

--- R/misc.R
+++ R/misc.R
@@ -1,7 +1,7 @@
 #' imagerExtra: Extra Image Processing Library Based on Imager
 #'
 #' imagerExtra is built on imager. imager by Simon Simon Barthelme provides an interface with CImg that is a C++ library for image processing. imager makes functions of CImg accessible from R and adds many utilities for accessing and working with image data from R.
-#' imagerExtra provides several advanced functions based on imager.
+#' imagerExtra provides advanced functions for image processing based on imager.
 #' @docType package
 #' @name imagerExtra
 NULL
